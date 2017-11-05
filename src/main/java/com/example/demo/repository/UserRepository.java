20665ffad33e4520286b1a1047bc4159e13f22d1
heiqie <heiqie@wacai.com>
2017-11-05 10:53:29 +0800

    fix issue#1 add annotation

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,7 +9,13 @@ import java.util.List;
  * Created by heiqie on 2017/9/22.
  */
 public interface UserRepository extends ElasticsearchRepository<User,String> {
+    /**
+     * 默认只返回10条
+     * @param name
+     * @return
+     */
     List<User> findByName(String name);
+
 
     List<User> deleteByName(String name);
 }
